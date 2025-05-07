312e3ef18ca3c8244d492820ca3c584ce8ea29f7
Adeola Samuel <meetprecious1403@gmail.com>
2025-05-07 17:37:52 +0100

    add rust projects

---
+++ hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d
@@ -0,0 +1,5 @@
+/home/adeola/Projects/Playground/Rust/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2: src/main.rs
+
+/home/adeola/Projects/Playground/Rust/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/adeola/Projects/Playground/Rust/hello_cargo/target/debug/hello_cargo: /home/adeola/Projects/Playground/Rust/hello_cargo/src/main.rs
