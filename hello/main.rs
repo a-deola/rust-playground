3baf48a23d60d2de928b592b3fb9c471031af0f5
Adeola Samuel <meetprecious1403@gmail.com>
2025-06-21 21:53:22 +0100

    running sum in rs

--- hello/main.rs
+++ hello/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("Hello, world!");
-    loop_label();
-    reverse_array();
+    // println!("Hello, world!");
+    // loop_label();
+    // reverse_array();
+    println!("{:?}", running_sum(vec![1, 1, 1, 1, 1]))
 }
 fn loop_label() {
     let mut count = 0;
@@ -28,6 +29,13 @@ fn reverse_array() {
     let mut array = [1, 2, 3, 4];
 
     array.reverse();
-    // add your code herein
     println!("reversed array: {:?}", array);
+}
+fn running_sum(mut nums: Vec<i32>) -> Vec<i32> {
+    let mut sum = 0;
+    for i in 0..nums.len() {
+        sum += nums[i];
+        nums[i] = sum;
+    }
+    nums
 }
